50979fa1fde8c9ad7c8540a67e43d832d449ae25
danneggiasta <zlatanovic.nebojsa@yahoo.com>
2017-08-31 04:50:31 +0200

    overall changes, adding bootstrap 4 beta, etc

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -25,16 +25,16 @@
             <div class="row">
                 @foreach($productChunk as $product)
 
-                <div class="col-lg-4 col-md-6 mb-4 mx-auto">
+                <div class="col-lg-4 col-md-6 mb-4 p-2">
                     <div class="card h-100">
 
-                        <div class="card-block">
-                            <img class="card-img-top img-fluid mb-4" src="{{ $product->imagePath }}" alt="">
+                        <div class="card-block p-2">
+                            <img class="card-img-top mb-4" src="{{ $product->imagePath }}" alt="">
                             <h4 class="card-title"><a href="{{route('product.show', ['id' => $product->id])}}">{{ $product->title }}</a></h4>
                             <h5>$ {{ $product->price }}</h5>
                             <p class="card-text">{{ $product->description }}</p>
                         </div>
-                        <div class="card-footer">
+                        <div class="card-footer p-2">
                             <small class="text-muted">{{ $product->subcategory->name }}</small>
                             <a href="{{ route('product.addToCart', ['id' => $product->id]) }}" class="btn btn-success pull-right" role="button">Add to Cart</a>
                         </div>
